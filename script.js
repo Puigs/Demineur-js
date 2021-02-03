733573f23908172a2f77b8b2b8a9b4533097e31c
Puigs <brayan.puigsagur@epitech.eu>
2021-02-03 09:59:28 +0100

    Add some functions

--- script.js
+++ script.js
@@ -25,7 +25,18 @@ function init() {
     }
 
     function affichage() {
-        
+      var square = document.getElementsByTagName("td");
+      for (var y = 0; y < taille; y++) {
+          for (var i = 0; i < taille; i++) {
+              if (grille[y][i].hidden == true)
+                  square[(taille * y) + i].setAttribute("class", "hidden");
+              else if (grille[y][i].bomb)
+                  square[(taille * y) + i].setAttribute("class", "bomb");
+              else {
+              }
+            }
+        }
+    }
     }
 
     function marquer(i,j) {
@@ -37,7 +48,22 @@ function init() {
       }
       
       function detection(event){
-         
+        var i,j,k;
+        var cases = document.getElementsByTagName("td");
+        for(k=0;k<cases.length;k++){
+            if(cases[k]==event.target){
+                i = parseInt (k/taille);
+                j = k%taille;
+                switch(event.button){
+                    case 0 : 
+                        decouvrir(i,j);
+                        break;
+                    case 2 :
+                        marquer(i,j);
+                        break;
+                }
+            }
+        }
       }    
 
       function rien(event){
